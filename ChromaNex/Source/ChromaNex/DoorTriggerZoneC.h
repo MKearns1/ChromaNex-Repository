5a5f96d91af8150736d7912c58fec197a7f0aa02
MKearns01 <w1972476@westminster.ac.uk>
2025-05-06 12:04:54 +0100

    testfix

--- ChromaNex/Source/ChromaNex/DoorTriggerZoneC.h
+++ ChromaNex/Source/ChromaNex/DoorTriggerZoneC.h
@@ -18,14 +18,7 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
-	// Make the TriggerBox visible to Blueprints and the editor
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Trigger")
-	class UBoxComponent* Box;
 
 private:
-	UPROPERTY(EditDefaultsOnly, Category = "Components") class UBoxComponent* BoxComp;
-	UPROPERTY(EditDefaultsOnly, Category = "Components") class UStaticMeshComponent* BaseMesh;
 
-	UFUNCTION()
-	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
